c5703c520cc41034598e030a0e337840cd6482be
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-15 17:09:28 +0400

    feat(components): Added MAin component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Sidebar from "../Sidebar/Sidebar";
+import Chat from "../Chat/Chat";
+
+import classes from "./Main.module.css";
+
+function Main() {
+  return (
+    <div className={classes.general}>
+      <Sidebar />
+      <Chat />
+    </div>
+  );
+}
+
+export default Main;
