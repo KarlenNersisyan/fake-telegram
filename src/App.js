a31d5ee97ecd74525c482200e5f65c149d84b714
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-15 16:41:55 +0400

    fix(App): Change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React from "react";
+import { lazy, Suspense } from "react";
 
-function App() {
+import { useAuth } from "./hooks/useAuth.hook";
+const AuthenticatedApp = lazy(() =>
+  import("./components/AuthenticatedApp/AuthenticatedApp")
+);
+const UnAuthenticatedApp = lazy(() =>
+  import("./components/UnAuthenticatedApp/UnAuthenticatedApp")
+);
+
+import "./root.css";
+import "./App.css";
+import LoadSpinner from "./components/LoadSpinner/LoadSpinner";
+
+export function App() {
+  const { user } = useAuth();
+
+  if (user === null) {
+    return (
+      <div>
+        <LoadSpinner />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-     <h1>HI KARLEN</h1>
-    </div>
+    <>
+      <Suspense
+        fallback={
+          <div>
+            <LoadSpinner />
+          </div>
+        }
+      >
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      </Suspense>
+    </>
   );
 }
-
-export default App;
