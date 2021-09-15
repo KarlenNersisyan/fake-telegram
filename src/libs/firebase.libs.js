763b647360eeb8fca98298ef76c8f377a775cda1
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-15 16:25:33 +0400

    faet(libs): Added firebase config

---
+++ src/libs/firebase.libs.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  domain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  id: process.env.REACT_APP_FIREBASE_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  senderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+});
+
+export default app;
+export const db = firebase.database();
